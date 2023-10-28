34bb0953ca688cd4c54d276260dc753c55b71b66
shan5742 <asamshan456@gmail.com>
2023-10-28 11:38:10 +0100

    [theme] - fix layout shit when theme changes

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -1,9 +1,19 @@
 "use client"
 
 import { useTheme } from "next-themes"
+import { useEffect, useState } from "react"
 
 export function ModeToggle() {
-  const { setTheme, theme } = useTheme()
+  const [mounted, setMounted] = useState(false)
+  const { setTheme, theme, systemTheme } = useTheme()
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
 
   return (
     <button
