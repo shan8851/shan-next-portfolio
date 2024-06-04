3766457a5d1a979c525af5ca90403827df94805b
shan8851 <asamshan456@gmail.com>
2024-06-04 20:24:45 +0100

    [now] - update now page june 2024

--- app/now/page.tsx
+++ app/now/page.tsx
@@ -3,7 +3,7 @@ export default function Now() {
     <div className="prose dark:prose-invert mt-6 text-md">
       This page was inspired by the <a>now movement</a> by <a>@Sivers</a>
       <p>
-        last updated: <span className="font-extrabold">January 2024</span>
+        last updated: <span className="font-extrabold">June 2024</span>
       </p>
       <div className="my-2 text-lg">
         <h2>Work & Learning</h2>
@@ -14,45 +14,26 @@ export default function Now() {
           to delve back in, work hard and see where I can go!
         </p>
         <p>
-          <a href="https://cielo.finance/">Cielo</a>: Still working hard in the
+          <a href="https://app.cielo.finance/">Cielo</a>: Still working hard in the
           Cielo Finance project, we have shipped some big things lately and
           there is more to come. I&apos;m naturally very bullish on the project,
           the team and crypto in general. I&apos;m excited to see where we can
           take this.
         </p>
-        <p>
-          Cielo used python for the backend, I really want to start working more
-          accross the full stack again, so to do that here I need to get up to
-          speed with Python. I&apos;ve made a start, it&apos;s slow going so
-          far, but hopefully I can push on in the coming weeks
-        </p>
       </div>
       <div className="my-2 text-lg">
         <h2>Life</h2>
         <p>
-          Family Man: Striving to be the best dad and husband (to be) I can be,
-          while juggling work and other passions.
+          Working hard, being curious and striving for balance.
         </p>
         <p>
-          Constant Learner: Always exploring new ideas and projects to keep
-          things fresh.
-        </p>
-        <p>
-          Indie Dreamer: Fantasising about the one-person business life and
-          joining the ranks of Indie Hackers.
+          Starting to become attracted to generating some passive income streams, the future and all that jazz.
         </p>
       </div>
       <div className="my-2 text-lg">
         <h2>Balance</h2>
         <p>
-          I am trying to find a better balance between work and life, I have a
-          tendency to get obsessed with work and let it take over my life. I am
-          trying to be more mindful of this and make sure I am spending time
-          with my family and friends, and also taking time to relax and
-          recharge. I started playing 7-a-side football again, which is a great
-          way to get some exercise and socialise. Alongside this I am still
-          working hard with the thai boxing, fitness and technique are
-          improving, but I still have a long way to go.
+          Football, Squash, reading, friends & family
         </p>
       </div>
     </div>
